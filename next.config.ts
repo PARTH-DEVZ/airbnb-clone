b5dfc092d5ab08d7253f4acd044c3e257b82b964
PARTH <contactwithparth@gmail.com>
2026-03-02 00:59:24 +0530

    fix: Update nextAuth files location

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, 
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "res.cloudinary.com"],
